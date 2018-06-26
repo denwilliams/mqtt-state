0564a1b0b7a6519ec0069404636a7d3f3b280a11
Den Williams <work@denwilliams.net>
2018-06-26 20:47:47 +1000

    http port

--- http.js
+++ http.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const { register } = require('prom-client');
 
-exports.create = (rootState, rules) => {
+exports.create = (rootState, rules, port = 3000) => {
   const app = express();
 
   app.get('/state', (req, res) => res.json(Object.assign({}, rootState.getState(), rules.getState())));
   app.get('/metrics', (req, res) => res.type('txt').send(register.metrics()));
 
-  app.listen(3000, () => console.log('Listening on port 3000!'));
+  app.listen(port, () => console.log(`Listening on port ${port}`));
 };
