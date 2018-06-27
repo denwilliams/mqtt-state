18ee07a7ad88df25a9326f792460f77ec19c8b15
Den Williams <work@denwilliams.net>
2018-06-28 00:51:14 +1000

    fix for undefined

--- rule-types/pick.js
+++ rule-types/pick.js
@@ -2,6 +2,6 @@ const { distinctUntilChanged, map } = require('rxjs/operators');
 
 module.exports = (rule, reactive) => {
   return reactive.getBinding(rule.source)
-  .pipe(map(x => x[rule.field]))
+  .pipe(map(x => x ? x[rule.field] : null))
   .pipe(distinctUntilChanged());
 };
