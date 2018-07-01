9002fee893ce9451c01df62f70255ad0298eb809
Den Williams <work@denwilliams.net>
2018-07-01 21:06:59 +1000

    always pass filter

--- rule-types/filter.js
+++ rule-types/filter.js
@@ -4,6 +4,5 @@ module.exports = (rule, reactive) => {
   const matcher = new RegExp(rule.regexp);
 
   return reactive.getBinding(rule.source)
-  .pipe(filter(value => matcher.test(value)))
-  .pipe(distinctUntilChanged());
+  .pipe(filter(value => matcher.test(value)));
 };
