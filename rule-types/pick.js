76eec9fb8f29b8021054f6c95b71a0bec81136db
Den Williams <work@denwilliams.net>
2018-07-08 21:12:33 +1000

    allow nested fields for pick

--- rule-types/pick.js
+++ rule-types/pick.js
@@ -1,7 +1,15 @@
 const { distinctUntilChanged, map } = require('rxjs/operators');
 
 module.exports = (rule, reactive) => {
+  const path = rule.field.split('.');
+
   return reactive.getBinding(rule.source)
-  .pipe(map(x => x ? x[rule.field] : null))
+  .pipe(map(x => {
+    return path.reduce((obj, field) => {
+      if (obj === null || obj === undefined) return null;
+
+      return obj[field];
+    }, x);
+  }))
   .pipe(distinctUntilChanged());
 };
