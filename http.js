b997e833022b5f436a8529487aff3ec7b4d98261
Den Williams <work@denwilliams.net>
2018-07-08 23:00:51 +1000

    allow getting values directly

--- http.js
+++ http.js
@@ -5,6 +5,7 @@ exports.create = (rootState, rules, port = 3000) => {
   const app = express();
 
   app.get('/state', (req, res) => res.json(Object.assign({}, rootState.getState(), rules.getState())));
+  app.get('/state/*', (req, res) => res.json(rules.getState()[req.params[0]]));
   app.get('/metrics', (req, res) => res.type('txt').send(register.metrics()));
 
   app.listen(port, () => console.log(`Listening on port ${port}`));
