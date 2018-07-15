83d9cc4698424610ff974be48cb73bff51ac927e
Den Williams <work@denwilliams.net>
2018-07-15 10:46:57 +1000

    add delay to activity

--- rule-types/activity.js
+++ rule-types/activity.js
@@ -7,30 +7,39 @@ module.exports = (rule, reactive) => {
 
   const sources = rule.sources.map(s => reactive.getBinding(s));
 
-  return merge(...sources)
-  .pipe(bumper(interval))
-};
-
-function bumper(duration) {
-  return function mySimpleOperatorImplementation(source) {
-    return Observable.create(subscriber => {
-      let bumper;
-      const timeoutFn = () => {
-        subscriber.next(false);
-        clearTimeout(bumper);
-        bumper = undefined;
-      };
-      var subscription = source.subscribe(() => {
-        // console.log('bump')
-        if (!bumper) subscriber.next(true);
-        else clearTimeout(bumper);
-
-        bumper = setTimeout(timeoutFn, duration);
-      },
-      err => subscriber.error(err),
-      () => subscriber.complete());
-
-      return subscription;
-    });
+  let ready = !rule.delay;
+  if (rule.delay) {
+    setTimeout(() => ready = true, rule.delay);
   }
-}
+
+  return merge(...sources)
+  .pipe(bumper(interval));
+
+  // ------
+
+  function bumper(duration) {
+    return function mySimpleOperatorImplementation(source) {
+      return Observable.create(subscriber => {
+        let bumper;
+        const timeoutFn = () => {
+          subscriber.next(false);
+          clearTimeout(bumper);
+          bumper = undefined;
+        };
+        var subscription = source.subscribe(() => {
+          if (!ready) return;
+
+          // console.log('bump')
+          if (!bumper) subscriber.next(true);
+          else clearTimeout(bumper);
+
+          bumper = setTimeout(timeoutFn, duration);
+        },
+        err => subscriber.error(err),
+        () => subscriber.complete());
+
+        return subscription;
+      });
+    }
+  }
+};
