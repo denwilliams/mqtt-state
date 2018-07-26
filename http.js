9e32b2e033988b8a86e7156b113325efc2f91d87
Den Williams <work@denwilliams.net>
2018-07-26 22:27:15 +1000

    allow filtering via http

--- http.js
+++ http.js
@@ -4,9 +4,24 @@ const { register } = require('prom-client');
 exports.create = (rootState, rules, port = 3000) => {
   const app = express();
 
-  app.get('/state', (req, res) => res.json(Object.assign({}, rootState.getState(), rules.getState())));
+  app.get('/state', (req, res) => res.json(getState(req.query)));
   app.get('/state/*', (req, res) => res.json(rules.getState()[req.params[0]]));
   app.get('/metrics', (req, res) => res.type('txt').send(register.metrics()));
+
+  function getState(query) {
+    const state = {};
+    if (query.root) Object.assign(state, rootState.getState());
+    Object.assign(state, rules.getState());
+
+    if (!query.select) return state;
+
+    const selectors = query.select.split(',');
+
+    return selectors.reduce((obj, s) => {
+      obj[s] = state[s];
+      return obj;
+    }, {})
+  }
 
   return {
     start() {
