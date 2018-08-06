2111c61b697eb933bb451980debd1ef126fd51e9
Den Williams <work@denwilliams.net>
2018-08-06 22:57:17 +1000

    let time of day cross days

--- rule-types/timeofday.js
+++ rule-types/timeofday.js
@@ -5,6 +5,14 @@ const { distinctUntilChanged, map } = require('rxjs/operators');
 module.exports = (rule, reactive) => {
   const a = rule.values[0];
   const b = rule.values[1];
+  let a2 = a;
+  let b2 = b;
+
+  if (b < a) {
+    // crosses over to next day
+    a2 = a - 24;
+    b2 = b + 24;
+  }
 
   const outside = rule.outside || false;
 
@@ -12,9 +20,12 @@ module.exports = (rule, reactive) => {
   .pipe(map(value => {
     const dval = new Date(value);
     const timeOfDay = dval.getHours() + (dval.getMinutes() / 60);
-    return (outside)
-      ? timeOfDay < a || timeOfDay > b
-      : timeOfDay > a && timeOfDay < b;
+
+    if (outside) return (timeOfDay < a || timeOfDay > b2)
+      && (timeOfDay < a2 || timeOfDay > b);
+
+    return (timeOfDay > a && timeOfDay < b2)
+      || (timeOfDay > a2 && timeOfDay < b);
   }))
   .pipe(distinctUntilChanged());
 };
