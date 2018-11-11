fd5a212b1beeb4b353a24a3d33e204c72947bb8a
Den Williams <work@denwilliams.net>
2018-11-12 09:15:42 +1100

    add counter test

---
+++ rule-types/counter.test.js
@@ -0,0 +1,16 @@
+const test = require("ava");
+const { beforeEach } = require("./_test-helper");
+
+test.beforeEach(beforeEach);
+
+test("counts how many times the source emits (changes)", t => {
+  const { rootState, createRules } = t.context;
+  const rules = createRules([
+    { key: "output/counter", type: "counter", source: "root/input/counter" }
+  ]);
+
+  rootState.setValue("root/input/counter", 123);
+  rootState.setValue("root/input/counter", 456);
+
+  t.is(rules.getState()["output/counter"], 2);
+});
