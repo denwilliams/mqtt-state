0f32b76204c84483c8aeaba1caeadb0aa95fea90
Den Williams <work@denwilliams.net>
2019-01-20 22:59:45 +1100

    fix labelNames

--- metrics.js
+++ metrics.js
@@ -3,12 +3,8 @@ const { Gauge } = require("prom-client");
 exports.create = metricsList => {
   const metrics = {};
   metricsList.forEach(m => {
-    const { name, help, labels } = m;
-    const gauge = new Gauge({
-      name,
-      help,
-      labels
-    });
+    const { name, help, labelNames } = m;
+    const gauge = new Gauge({ name, help, labelNames });
     metrics[name] = gauge;
   });
   return metrics;
