3b126769e1f7e8132c995c4234a19f50ab92c61e
Den Williams <work@denwilliams.net>
2020-04-30 01:50:52 +1000

    redis persistence

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 import yaml from "js-yaml";
-import fs from "fs";
+import { readFile } from "fs";
 import { join } from "path";
+import { promisify } from "util";
+
 import { create as createFileState } from "./file-state";
+import { create as createRedisState } from "./redis-state";
 import { create as createRootState } from "./root-state";
 import { Mqtt } from "./mqtt";
 import { create as createReactive } from "./reactive";
@@ -12,42 +15,58 @@ import { create as createHttp } from "./http";
 import { create as createTicker } from "./ticker";
 import { RuleDetails, Config } from "./types";
 
-const configPath = process.env.CONFIG_PATH || __dirname + "/config.yml";
-const config: Config = yaml.safeLoad(fs.readFileSync(configPath, "utf8"));
+const readFileAsync = promisify(readFile);
 
-const filePath = config.data.file || join(__dirname, "state.json");
-const fileState = createFileState(filePath);
-const rootState = createRootState(fileState.load());
-
-rootState.subscribe(() => {
-  fileState.save(rootState.getState());
-});
-
-const reactive = createReactive(rootState);
-const mqtt = new Mqtt(
-  rootState,
-  config.mqtt.uri,
-  config.mqtt.subscriptions || [],
-  config.mqtt.raw || []
-);
-
-let rulesList: RuleDetails[] = [];
-for (const rule of config.rules) {
-  if (rule.import === undefined) {
-    rulesList.push(rule);
-    continue;
+function getPersistence(config: { data: { redis?: string; file?: string } }) {
+  if (config.data.redis) {
+    return createRedisState(config.data.redis);
   }
-  const imported = yaml.safeLoad(fs.readFileSync(rule.import, "utf8"));
-  rulesList = rulesList.concat(imported);
+  const filePath = config.data.file || join(__dirname, "state.json");
+  return createFileState(filePath);
 }
 
-const metrics = createMetrics(config.metrics);
-const rules = new Rules(rulesList, reactive, mqtt, metrics);
-const http = createHttp(rootState, rules, config.http.port);
-const ticker = createTicker(rootState, reactive);
+async function main() {
+  const configPath = process.env.CONFIG_PATH || __dirname + "/config.yml";
+  const config: Config = yaml.safeLoad(await readFileAsync(configPath, "utf8"));
 
-mqtt
-  .start()
-  .then(() => rules.start())
-  .then(() => http.start())
-  .then(() => ticker.start());
+  const persistence = getPersistence(config);
+  const rootState = createRootState(await persistence.load());
+
+  rootState.subscribe(() => {
+    persistence.save(rootState.getState());
+  });
+
+  const reactive = createReactive(rootState);
+  const mqtt = new Mqtt(
+    rootState,
+    config.mqtt.uri,
+    config.mqtt.subscriptions || [],
+    config.mqtt.raw || []
+  );
+
+  let rulesList: RuleDetails[] = [];
+  for (const rule of config.rules) {
+    if (rule.import === undefined) {
+      rulesList.push(rule);
+      continue;
+    }
+    const imported = yaml.safeLoad(await readFileAsync(rule.import, "utf8"));
+    rulesList = rulesList.concat(imported);
+  }
+
+  const metrics = createMetrics(config.metrics);
+  const rules = new Rules(rulesList, reactive, mqtt, metrics);
+  const http = createHttp(rootState, rules, config.http.port);
+  const ticker = createTicker(rootState, reactive);
+
+  await mqtt
+    .start()
+    .then(() => rules.start())
+    .then(() => http.start())
+    .then(() => ticker.start());
+}
+
+main().catch((err) => {
+  console.error(err.stack);
+  process.exit(1);
+});
